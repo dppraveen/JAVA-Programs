9a72616997cd21a5666202638117da21d20dbf81
Praveen Kumar <55030389+dppraveen@users.noreply.github.com>
2021-04-28 18:05:56 +0530

    Create IfElseDemo.java

---
+++ IfElseDemo.java
@@ -0,0 +1,14 @@
+class IfElseDemo{
+	public static void main(String[] args) {
+		// int var = 87;//Hardcoded value, not recomended
+
+		boolean var = false;
+
+		if(var){ //Works this time with a boolean
+			System.out.println("YES");
+		}
+		else{
+			System.out.println("NO");
+		}
+}
+}
