7036907eb8eef80e47766afc60fd3a04d0ee6661
Praveen Kumar <55030389+dppraveen@users.noreply.github.com>
2021-04-28 17:04:22 +0530

    Create Sum100.java

---
+++ Sum100.java
@@ -0,0 +1,9 @@
+class Sun100{
+  public static void main(String[] args){
+    int i,sum=o;
+    for(i=0;i<=100;i++){
+      sum+=i;
+    }
+    System.out.println("Sum of 100 numbers is:"+sum);
+  }
+}
