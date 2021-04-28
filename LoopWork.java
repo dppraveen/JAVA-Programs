d4ac4735833780e2d3ff7d91145996ec0aa002d4
Praveen Kumar <55030389+dppraveen@users.noreply.github.com>
2021-04-28 18:06:51 +0530

    Create LoopWork.java

---
+++ LoopWork.java
@@ -0,0 +1,62 @@
+class LoopWork{
+	public static void main(String[] args) {
+		// for(int var = 75; var >= 55; var -= 2){
+		// 	System.out.println(var);
+		// }
+		// int var = 75;
+		// while(var >= 55){
+		// 	System.out.println(var);
+		// 	var -= 2;
+		// }
+
+		// int value = new java.util.Scanner(System.in).nextInt(), rev = 0;
+
+		// for(; value > 0; ){
+		// 	rev = value % 10;
+		// 	value /= 10;
+		// 	System.out.print(rev);
+		// }
+
+		// OR with a While loop
+		// while(value > 0){
+		// 	rev = value % 10;
+		// 	value /= 10;
+		// 	System.out.print(rev);
+		// }
+		int var = 0;
+
+		do{
+			System.out.println(var);
+			var++;
+		}while(var >= 10);	
+
+	
+
+		// 1
+		// 1
+		// 2
+		// 3
+		// 5
+		// 8
+		// 13
+		// 21
+		// 34
+		// 55
+		// 89
+
+
+
+		// Sum of first 100 numbers
+
+		// Factorial
+
+		// 6
+		// 6*5*4*3*2*1 = 720
+
+
+
+
+
+
+	}
+}
