ba2ca6e79a9b3634f25716013524bb04be3edd11
Praveen Kumar <55030389+dppraveen@users.noreply.github.com>
2021-04-28 17:52:02 +0530

    Create MultiplyWithParametersAndReturnTypes.java

---
+++ MultiplyWithParametersAndReturnTypes.java
@@ -0,0 +1,19 @@
+class MultiplyWithParametersAndReturnTypes{
+	public static void main(String[] args) {
+		java.util.Scanner theScan = new java.util.Scanner(System.in);
+
+		System.out.println("Enter two numbers to multiply:");
+
+		int firstValue = theScan.nextInt();	
+		int secondValue = theScan.nextInt();
+
+		System.out.println("Result is : " + new MultiplyWithParametersAndReturnTypse().multiply(firstValue, secondValue));
+	
+	}
+
+
+	int multiply(int firstValue, int secondValue){
+		
+		return (first * second);
+	}
+}
