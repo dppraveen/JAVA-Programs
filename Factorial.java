89a3e9faa8eed6e2a762508ab3d2fa09e51fc72a
Praveen Kumar <55030389+dppraveen@users.noreply.github.com>
2021-04-28 17:13:50 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,9 @@
+class Factorial{
+  public static void main(String[] args){
+    int x=1,i,n=6;
+    for(i=1;i<=n;i++){
+      x=x*i;
+    }
+    System.out.println("Factorial of a number is:"+x);
+  }
+}
