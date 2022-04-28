de677604c4998e499c97ee883add37298474afa6
Praveen Kumar <55030389+dppraveen@users.noreply.github.com>
2022-04-28 17:52:38 +0530

    Add files via upload
    Assignment 1: Taking input from user and printing output using only if statements.
    take 3 user inputs, let's say a, b, & c and get their sum. Now, if a, b & c should >= 40, print as "Student is Passing" or print as "Student is Failing". 
    and check if their is sum is >=125 and print as "Student is Passing" or print as "Student is Failing".
    NOTE: Using if statements alone, no nested if or if else statements.

--- TakingInput.java
+++ TakingInput.java
@@ -1,26 +1,33 @@
-class TakingInput{
-   public static void main(String args[]){
-    int a=Integer.parseInt(args[0]);
-    int b=Integer.parseInt(args[1]);
-    int c=Integer.parseInt(args[2]);   
-    int sum =a+b+c;
-int result=1;
-if(a<40){
-	result=0;
-}
-if(b<40){
-	result=0;
-}
-if(c<40){
-	result=0;
-}
-if(sum<125){
-	result=0;
-}
-if(result==1){
-	System.out.println("Student has Passed");
-else{
-	System.out.println("Student has Failed");
-}
+public class TakingInput
+{
+	public static void main(String[] args) {
+	    int a=Integer.parseInt(args[0]);
+	    int b= Integer.parseInt(args[1]);
+	    int c= Integer.parseInt(args[2]);
+	    int sum= a+b+c;
+	if(a>=40){
+		System.out.println("Pass");	
+	}
+	if(a<40){
+		System.out.println("Fail");	
+	}
+	if(b>=40){
+		System.out.println("Pass");	
+	}
+	if(b<40){
+		System.out.println("Fail");	
+	}
+	if(c>=40){
+		System.out.println("Pass");	
+	}
+	if(c<40){
+		System.out.println("Fail");	
+	}
+	if(sum>=125){
+		System.out.println("Pass");	
+	}
+	if(sum<125){
+		System.out.println("Fail");	
+	}
 }
 }
